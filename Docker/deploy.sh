fd8e4ae65858ec4e3fd52811ca6297ebcef227c0
Amanda Leandro Soares Do Carmo <41752743+amandaleandro@users.noreply.github.com>
2024-03-20 18:57:18 -0300

    Update deploy.sh

--- Docker/deploy.sh
+++ Docker/deploy.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Definir o nome da imagem e da tag
 NOME_IMAGEM="amanda-nigixnds1"
 TAG="latest"
 Nome_Container="container_testnds1"
 
-# Construir a imagem Docker
+
 echo "Construindo a imagem Docker..."
 docker build -t $NOME_IMAGEM:$TAG .
 
-# Verificar se a construção da imagem foi bem-sucedida
 if [ $? -eq 0 ]; then
     echo "Imagem Docker construída com sucesso: $NOME_IMAGEM:$TAG"
 
